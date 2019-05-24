052ae6809467708c628bac00b7ba47480b40f7f8
luc.demeyer@noviat.com <luc.demeyer@noviat.com>
2019-05-24 17:10:31 +0200

    use _select_seller method

--- base_ubl/models/ubl.py
+++ base_ubl/models/ubl.py
@@ -342,10 +342,9 @@ class BaseUbl(models.AbstractModel):
         if product:
             if type == 'purchase':
                 if seller:
-                    sellers = self.env['product.supplierinfo'].search([
-                        ('name', '=', seller.id),
-                        ('product_tmpl_id', '=', product.product_tmpl_id.id),
-                        ('product_id', '=', product.id)])
+                    sellers = product._select_seller(
+                        partner_id=seller, quantity=0.0, date=None,
+                        uom_id=False)
                     if sellers:
                         product_name = sellers[0].product_name
                         seller_code = sellers[0].product_code
